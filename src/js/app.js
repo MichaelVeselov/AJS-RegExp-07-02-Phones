67837a9a0c6f16bd79ae414e1e8c2c492551bc8a
Michael Veselov <michael.veselov@gmail.com>
2022-04-19 10:23:00 +0300

    deleted comments from app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,4 @@
 export default function phoneNumberMatcher(phoneNumber) {
-  // phoneNumber = phoneNumber.replace(/^8/, '7');
   const regExp = /\d/g;
   const matchedPhoneNumber = phoneNumber.replace(/^8/, '7').match(regExp).join('');
   return `+${matchedPhoneNumber}`;
